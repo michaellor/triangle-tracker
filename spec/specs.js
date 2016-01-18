56e21d536aeaecf13ddeebfeb6d24c1d602e7b07
Michael Lor <michael.vang.1985@gmail.com>
2016-01-18 14:43:04 -0800

    set up files and folders

---
+++ spec/specs.js
@@ -0,0 +1,25 @@
+describe('leapYear', function() {
+  it("is false for a year that is not divisible by 4, 100, or 400", function() {
+    expect(leapYear(1993)).to.equal(false);
+  });
+
+  // it("is false for an input that is not a number", function() {
+  //   expect(isNaN((leapYear))).to.be.false;
+  // });
+
+  it("is false for an input that is less than 0", function() {
+    expect(leapYear(-29)).to.equal(false);
+  })
+
+  it("is true for years divisible by 4", function() {
+    expect(leapYear(2004)).to.equal(true);
+    });
+
+  it("is true for years divisible by 100", function() {
+    expect(leapYear(1900)).to.equal(false);
+    });
+
+  it("is true for years divisible by 400", function() {
+    expect(leapYear(2000)).to.equal(true);
+  });
+});
