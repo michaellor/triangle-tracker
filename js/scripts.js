0921b43746a5b441dd0fb090a068d66ee5aa3a3a
Michael Lor <michael.vang.1985@gmail.com>
2016-01-18 15:40:45 -0800

    add specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,15 @@
-var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else if (year < 0) {
+var triangle = function(a,b,c) {
+
+  if ((a + b <= c) || (b + c <= a) || (a + c <= b)) {
     return false;
+  } else if (a === b && b === c && c === a) {
+    return true;
+  } else if (a === b || b === c || c === a) {
+    return true;
+  } else if (a !== b && b !== c && c !== a) {
+    return true;
   }
   else {
     return false;
   }
-};
-
-
-$(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-
-    $(".year").text(year);
-    if (!result) {
-      $(".not").text("not");
-  } else {
-      $(".not").text("");
-  }
-
-    $("#result").show();
-    event.preventDefault();
-  });
-});
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,25 +1,40 @@
-describe('leapYear', function() {
-  it("is false for a year that is not divisible by 4, 100, or 400", function() {
-    expect(leapYear(1993)).to.equal(false);
+describe('triangle', function() {
+
+  it("is false for the sum of any two sides to be less than or equal to the length of the third side.", function() {
+    expect(triangle(2,2,4)).to.equal(false);
   });
+
+  it("is true for an equilateral to have all sides equal.", function() {
+    expect(triangle(2,2,2)).to.equal(true);
+  });
+
+  it("is true for an isosceles to have two sides that are equal.", function() {
+    expect(triangle(2,2,1)).to.equal(true);
+  });
+
+  it("is true for a scalene triangle to have all three sides not equal.", function() {
+    expect(triangle(2,3,4)).to.equal(true);
+  });
+
+});
 
   // it("is false for an input that is not a number", function() {
   //   expect(isNaN((leapYear))).to.be.false;
   // });
 
-  it("is false for an input that is less than 0", function() {
-    expect(leapYear(-29)).to.equal(false);
-  })
-
-  it("is true for years divisible by 4", function() {
-    expect(leapYear(2004)).to.equal(true);
-    });
-
-  it("is true for years divisible by 100", function() {
-    expect(leapYear(1900)).to.equal(false);
-    });
-
-  it("is true for years divisible by 400", function() {
-    expect(leapYear(2000)).to.equal(true);
-  });
-});
+//   it("is false for an input that is less than 0", function() {
+//     expect(leapYear(-29)).to.equal(false);
+//   })
+//
+//   it("is true for years divisible by 4", function() {
+//     expect(leapYear(2004)).to.equal(true);
+//     });
+//
+//   it("is true for years divisible by 100", function() {
+//     expect(leapYear(1900)).to.equal(false);
+//     });
+//
+//   it("is true for years divisible by 400", function() {
+//     expect(leapYear(2000)).to.equal(true);
+//   });
+// });
